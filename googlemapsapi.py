2ffdf6653f68b647126509fb91d4163daeabd2d8
nleobandung <nathan.leobandung@gmail.com>
2024-04-20 16:01:25 -0700

    function framework

--- googlemapsapi.py
+++
@@ -1,5 +0,0 @@
-import googlemaps
-import json
-
-API_KEY = "AIzaSyBF8q9l9Zv1wqZVUFmXmsV5Ohs0NmfSzto"
-gmaps = googlemaps.Client(key=API_KEY)
---
+++ py_files/googlemapsapi.py
@@ -0,0 +1,18 @@
+import googlemaps
+import json
+
+API_KEY = "AIzaSyBF8q9l9Zv1wqZVUFmXmsV5Ohs0NmfSzto"
+gmaps = googlemaps.Client(key=API_KEY)
+
+def geocode(origin, destination):
+    return
+
+def find_route(lat1, lon1, lat2, lon2):
+    return
+
+def find_elevation():
+    return
+
+def find_places():
+    return
+
