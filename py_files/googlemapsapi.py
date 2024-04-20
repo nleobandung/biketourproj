a1ea970fd5215352120b2517923bbf338ad184b1
nleobandung <nathan.leobandung@gmail.com>
2024-04-20 16:26:10 -0700

    geocode

--- py_files/googlemapsapi.py
+++ py_files/googlemapsapi.py
@@ -7,7 +7,10 @@ API_KEY = "AIzaSyBF8q9l9Zv1wqZVUFmXmsV5Ohs0NmfSzto"
 gmaps = googlemaps.Client(key=API_KEY)
 
 def geocode(origin, destination):
-    return
+    geocode_origin = (gmaps.geocode(origin))[0]["geometry"]["location"]
+    geocode_dest = (gmaps.geocode(destination))[0]["geometry"]["location"]
+
+    return geocode_origin["lat"], geocode_origin["lng"], geocode_dest["lat"], geocode_dest["lng"]
 
 def find_route(lat1, lon1, lat2, lon2):
     return
@@ -18,3 +21,5 @@ def find_elevation():
 def find_places():
     return
 
+
+print(geocode("3202 Route 52 Stormville NY 12582", "New York City"))
