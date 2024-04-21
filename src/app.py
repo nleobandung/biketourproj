7dfb7b816209911eb7becfc67c49ec0e8a7a8ac0
nleobandung <nathan.leobandung@gmail.com>
2024-04-20 20:10:07 -0700

    added api route finder call

--- src/app.py
+++ src/app.py
@@ -1,17 +1,19 @@
 from flask import Flask,jsonify,request
+from datetime import datetime
+import googlemaps
 
 app = Flask(__name__)
 
 @app.route('/returnjson', methods=['GET']) 
 def ReturnJSON(): 
+    API_KEY = "AIzaSyBF8q9l9Zv1wqZVUFmXmsV5Ohs0NmfSzto"
+    gmaps = googlemaps.Client(key=API_KEY)
     pos1 = request.args.get('pos1')
     pos2 = request.args.get('pos2')
-    data = { 
-        "Modules" : pos1, 
-        "Subject" : pos2, 
-    } 
-  
-    return jsonify(data) 
+    now = datetime.now()
+    directions_result = gmaps.directions(pos1,pos2,mode="bicycling",departure_time = now)
+    directions_result = directions_result[0]["legs"][0]["steps"][0]
+    return jsonify(directions_result) 
     
 if __name__ == "__main__":
     app.run(port=3000)
